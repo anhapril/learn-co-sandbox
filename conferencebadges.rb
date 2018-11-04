530d4235f95bb9d56b59919c65fcdd6ae44dc2e5
Alida Hines <alida.hines@gmail.com>
2018-11-04 21:06:33 +0000

    Done.

---
+++ conferencebadges.rb
@@ -0,0 +1,15 @@
+def badge_maker(name)
+puts "Hello, my name is (name)"
+end
+
+def batch_badge_creator(names)
+#.each
+[“Edsger","Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+7.times do |names|
+puts “Hi, #{name}”
+end
+
+def assign_rooms (names,room_number)
+(assign_rooms). each_with_index {|item, index|
+  hash[item] = index
+end
